c6ded19fbc1856f8bc64d87294fc93622ceb8dc1
Fadi Asaad <firon1222@gmail.com>
2024-05-17 01:43:29 +0300

    add task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Salt and hash password """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check and validate password """
+    return bcrypt.checkpw(password.encode(), hashed_password)
