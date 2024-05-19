b440de199248bfea950fce9231557243d6fbcc56
Fadi Asaad <firon1222@gmail.com>
2024-05-19 04:03:14 +0300

    fix: Task 2: use logger.getLogger instead of creating a new logger using constructor

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,12 +24,14 @@ def filter_datum(
 
 def get_logger() -> logging.Logger:
     """ create logger and return back """
-    logger = logging.Logger("user_data")
+    logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
+
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
     logger.addHandler(handler)
+
     return logger
 
 
