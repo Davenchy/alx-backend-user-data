93a5b978afca63e5bb25764124fb8a447664ceac
Fadi Asaad <firon1222@gmail.com>
2024-05-22 04:50:45 +0300

    add task 8: BasicAuth.decode_base64_authorization_header method and demo file for it

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of BasicAuth
 """
 from .auth import Auth
+from base64 import decodebytes
 
 
 class BasicAuth(Auth):
@@ -11,8 +12,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
-        """ extracts the base64 token from authorization http header
-        """
+        """ extracts the base64 token from authorization http header """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -20,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ decode base64 authorization http header token """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = decodebytes(base64_authorization_header.encode('utf-8'))
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
