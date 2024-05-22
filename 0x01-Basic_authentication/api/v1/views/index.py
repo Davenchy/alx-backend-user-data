6d77620fc5e5cc63c4e87f515429b10554afa41b
Fadi Asaad <firon1222@gmail.com>
2024-05-22 03:41:48 +0300

    add task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauth() -> str:
       - 401
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - 403
+    """
+    abort(403)
