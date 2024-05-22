af02ad8180c92078c91a53ddd7346449e39f7f26
Fadi Asaad <firon1222@gmail.com>
2024-05-22 03:49:13 +0300

    add task 3: init auth management class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module of Auth
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Auth Management Class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ set path as authentication required """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ get the current request authentication header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar['User']:
+        """ get the current user detected by the authentication process """
+        return None
