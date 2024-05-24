c55848e6408587934040a66a48180d89a97529d6
Fadi Asaad <firon1222@gmail.com>
2024-05-24 03:49:20 +0300

    add task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,7 +4,18 @@
 from .auth import Auth
 from typing import List, TypeVar
 from flask import request
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Session Auth Management Class """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a new session for user_id """
+        if not user_id or type(user_id) is not str:
+            return None
+        
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
