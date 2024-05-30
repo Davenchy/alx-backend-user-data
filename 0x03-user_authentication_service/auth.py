255ed7195baffb7d707a418e0bf6d6f1ffef93e4
Fadi Asaad <firon1222@gmail.com>
2024-05-31 01:58:10 +0300

    update: add Auth.valid_login method to validate user login credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,7 @@
 Auth module
 """
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
@@ -42,3 +42,11 @@ class Auth:
         # hash the password
         hashed_password = _hash_password(password)
         return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validate if the email and password match the database. """
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
