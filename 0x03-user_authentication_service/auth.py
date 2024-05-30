e337d67c70025145ea3727d397e938a4571783bc
Fadi Asaad <firon1222@gmail.com>
2024-05-31 02:04:51 +0300

    fix: the _generate_uuid method must be under the auth module not the Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,16 +17,17 @@ def _hash_password(password: str) -> bytes:
     return hashpw(password.encode('utf-8'), gensalt())
 
 
+def _generate_uuid() -> str:
+    """ Generate a new UUID """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _generate_uuid(self) -> str:
-        """ Generate a new UUID """
-        return str(uuid4())
 
     def register_user(self, email: str, password: str) -> User:
         """ Register a new user to the database.
