2b557b4efaeacff1b25e6d2d2db1c4c56911359e
Fadi Asaad <firon1222@gmail.com>
2024-05-31 01:53:04 +0300

    update: app: add POST /users route to register new users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Setup Basic Flask App"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", strict_slashes=False)
@@ -11,6 +13,25 @@ def root():
     """ GET /
     Returns jsonified message """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ POST /users
+    Expects 2 form fields email and password
+
+    Returns:
+        200: on success
+        400: on failure"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
